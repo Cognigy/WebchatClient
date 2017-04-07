3e91bf33cf5fb474e11f2421c8888fa08fcb4ec0
Mayr Benjamin <mail@benjamin-mayr.com>
2017-04-07 11:59:11 +0200

    Add missing exports to "index.ts". All classes and interfaces are now exported correctly.

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
+// all interfaces and helpers
+export {CognigyError} from "./src/interfaces/cognigyError";
+export {Options} from "./src/interfaces/options";
+export {Output} from "./src/interfaces/output";
+
 // the actual web client
+export {CognigyClient} from "./src/client";
 export {CognigyWebClient} from "./src/webClient";
