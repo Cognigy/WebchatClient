ef0b7c0fedc8d44a73f1bb5a0a8a2b88505417bd
monty0157 <k.zederkof@cognigy.com>
2017-07-27 17:41:11 +0200

    fixed bug: output.text.replace is not a function

--- src/client.ts
+++ src/client.ts
@@ -390,9 +390,7 @@ export class CognigyClient {
 
 		this.mySocket.on("output", (output: Output) => {
 			if (!this.options.keepMarkup) {
-				try {
-					output.text = (output.text) ? output.text.replace(/<[^>]*>/g, "") : output.text;
-				} catch (err) { }
+			    output.text = (output && output.text && typeof output.text === "string") ? output.text.replace(/<[^>]*>/g, "") : output.text;
 			}
 
 			this.options.handleOutput ? this.options.handleOutput(output) : console.log("Text: " + output.text + " Data: " + output.data);
